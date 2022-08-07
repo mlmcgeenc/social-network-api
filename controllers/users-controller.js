01cf9b6ed0a286eb9de6d1d1a8ba992ed879d107
Matt McGee <mlmcgeenc@gmail.com>
2022-08-07 17:59:04 -0400

    correct errors in 404 of users-controller.js

---
+++ controllers/users-controller.js
@@ -0,0 +1,77 @@
+const { User } = require('../models');
+
+const userController = {
+	// get all users
+	getAllUsers(req, res) {
+		User.find({})
+			.populate({
+				path: 'thoughts',
+				select: '-__v',
+			})
+			.select('-__v')
+			.sort({ __id: -1 })
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
+	// get one user by id
+	getUserById({ params }, res) {
+		User.findOne({ __id: params.id })
+			.populate({
+				path: 'comments',
+				select: '-__v',
+			})
+			.select('-__v')
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: 'There is no user with the provided id in the database.' });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
+	// create a new user
+	createUser: function ({ body }, res) {
+		User.create(body)
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => res.status(400).json(err));
+	},
+	// update a user by id
+	updateUser: function ({ params, body }, res) {
+		User.findOneAndUpdate({ __id: params.id }, body, { new: true, runValidators: true })
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: 'There is no user with the provided id in the database.' });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
+	// delete a user by id
+	deleteUser({ params }, res) {
+		User.findOneAndDelete({ __id: params.id })
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: 'There is no user with the provided id in the database.' });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
+};
+
+module.exports = userController;
